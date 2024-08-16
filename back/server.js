32234a0a3a66959cbcb79ae3a76bcd9c5fc17c10
junemo <junemo010107@gmail.com>
2024-08-16 16:25:39 +0900

    react revised

---
+++ back/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app=express();
+
+const PORT=process.env.PORT || 4000;
+
+const db=mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '0107',
+    database: 'participants'
+});
+
+db.connect(err => {
+    if(err){
+        console.error('MySQL 연결 에러:', err);
+        return;
+    }
+    console.log('MySQL 연결 성공');
+})
+
+//데이터 저장하는 엔드포인트
+app.post('/api/participants', (req, res) => {
+    const { phoneNumber} = req.body;
+  
+    const query = 'INSERT INTO participants (phoneNumber) VALUES (?)';
+    db.query(query, [phoneNumber], (err, result) => {
+      if (err) {
+        console.error('데이터베이스 저장 오류:', err);
+        res.status(500).json({ error: 'Database error' });
+        return;
+      }
+      res.status(201).json({ message: 'Participant added successfully', id: result.insertId });
+    });
+  });
+
+// 데이터 조회하는 엔드포인트
+app.get('/api/participants', (req, res) => {
+    db.query('SELECT * FROM participants', (err, results) => {
+      if (err) {
+        console.error('데이터베이스 조회 오류:', err);
+        res.status(500).json({ error: 'Database error' });
+        return;
+      }
+      res.status(200).json(results);
+    });
+  });
+
+app.get('/',(req,res) =>{
+    res.send("Server Success");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server run : http://localhost:${PORT}/`)
+})
+
